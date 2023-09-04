435cb7b26db287d8398b5499ebe0350fb2cf28d2
javiscripto <javier.mecker94@gmail.com>
2023-09-04 11:13:28 -0300

    funcionalidad lightbox.js y agrego estilos

---
+++ JS/lightbox.js
@@ -0,0 +1,27 @@
+const imagenes= document.querySelectorAll(".imagen")
+const display=document.querySelector(".displayOff")
+
+imagenes.forEach(imagen=>{
+    imagen.addEventListener("click",(e)=>{
+   let srcValue=imagen.getAttribute("src");
+
+   display.innerHTML=`
+   <img src="${srcValue}" alt="" class="img-show">
+    <img src="/imagenes/bx-x.svg" alt="" id="close">
+   `;
+   display.classList.toggle("displayOn")
+   
+
+   //cerrar ventana
+   const close=document.getElementById("close");
+   close.addEventListener("click",()=>{
+    display.classList.remove("displayOn")
+   })
+})
+})
+
+
+
+
+
+
